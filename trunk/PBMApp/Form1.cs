8a0440f5b66b9c9a40f05af4a9600c96f4bfcde7
koorise@gmail.com <koorise@gmail.com@865599a0-8436-2257-0c61-9e06b59a9790>
2012-05-08 12:37:45 +0000

    PBMApp初始化， root目录下的pbm.db为sqlite3文件。

---
+++ trunk/PBMApp/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SubSonic;
+using PBM.DAL;
+using SubSonic.Query;
+
+namespace PBMApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var q = from c in Plot.All()
+            //        orderby c.Id ascending 
+            //        select c;
+
+            PLU p=new PLU();
+            
+
+        }
+    }
+}
